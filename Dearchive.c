54609d9b57709d5863c51f4947fdfc9ad1053622
zayak <zaya.kor@mail.ru>
2022-04-02 21:22:47 +0300

    Добавила CMakeLists

--- Dearchive.c
+++ Dearchive.c
@@ -1,13 +1,8 @@
 //
 // Created by zayak on 01.04.2022.
 //
-
+#include <stdlib.h>
 #include <stdio.h>
-main(){
-
-    a = 3;
-    b = 9;
-    a = a + b;
-    printf('%d', &a);
+int main(){
     return 0;
 }
